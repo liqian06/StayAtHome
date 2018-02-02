6e6b318c76910e46ba61a3236665b9895238d6ce
test <test>
2018-02-02 21:36:27 +0800

    允典居家 ---路由 -----黎倩

--- stayathome/src/app/app-routing.module.ts
+++ stayathome/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { EatComponent } from './eat/eat.component';
+import { FindComponent } from './find/find.component';
+import { MyComponent } from './my/my.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"layout",pathMatch:'full'},
+  {path:"layout",component:LayoutComponent,
+  children:[
+      {path:"",redirectTo:"home",pathMatch:'full'},
+      {path:"home",component:HomeComponent},
+      {path:"eat",component:EatComponent},
+      {path:"find",component:FindComponent},
+      {path:"my",component:MyComponent}
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- stayathome/src/app/app.module.ts
+++ stayathome/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EatComponent } from './eat/eat.component';
+import { FindComponent } from './find/find.component';
+import { MyComponent } from './my/my.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EatComponent,
+    FindComponent,
+    MyComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
