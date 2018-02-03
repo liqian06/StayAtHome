32cd0955ba3d8a832c2250ad85c0b9d67e05a0bc
daisy <1063713699@qq.com>
2018-02-03 11:49:49 +0800

    允典  my   first   谢诗慧

--- stayathome/src/app/app.module.ts
+++ stayathome/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpModule} from "@angular/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +24,11 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
