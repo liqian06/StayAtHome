a60b2f4b1d7f9b245c5bf861e75ac2c37995509e
1373720368@qq.com <1373720368@qq.com>
2018-02-05 13:02:32 +0800

    允典居家----eat页面----张世明

--- stayathome/src/app/eat/eat.component.ts
+++ stayathome/src/app/eat/eat.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EatComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+ 
+  }
 
   ngOnInit() {
+       //获得slider插件对象
+       var gallery = mui('.mui-slider');
+       gallery.slider({
+         interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+       });
   }
 
 }
